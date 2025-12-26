f86534a2077a7ae8c44f74ba290cdb06b3b63f4e
Danila <danilakopylov603@gmail.com>
2025-12-26 22:13:36 +0800

    add

---
+++ src/main/java/ru/kopylov/multithreads/MultithreadsApplication.java
@@ -0,0 +1,13 @@
+package ru.kopylov.multithreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kopylov/multithreads/MultithreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kopylov.multithreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
